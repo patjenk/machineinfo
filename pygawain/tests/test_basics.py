1f92fdb90597e27f47111b9b3d84b78c46df6a37
Patrick Jenkins <patrick@socialgrowthtechnologies.com>
2014-04-28 13:31:32 +0000

    adding the ability to pull in network interfaces using ifconfig. The test for this need more work but will do for now.

--- pygawain/tests/test_basics.py
+++ pygawain/tests/test_basics.py
@@ -16,13 +16,22 @@ class GawainBasicsTestCase(TestCase):
         Test that we can identify the number of processors and their types.
         """
         processor = Gawain()
-
         read_data=open('./pygawain/tests/staticfiles/proc_cpuinfo.example', 'r').read()
         with patch('__builtin__.open', create=True) as mymock:
             mymock.return_value = MagicMock(spec=file)
             handle = mymock.return_value.__enter__.return_value
             handle.__iter__.return_value = iter(read_data.split("\n"))
-
-
             processors = processor._cpudata()
         self.assertTrue("1234 PJ" in processors['cpu_model'])
+
+    def test_network_interfaces(self):
+        """
+        Test that we can successfully find the network interfaces
+
+        TODO: mock out the call to ifconfig to and inspect the result from 
+        _networkdata better.
+        """
+        processor = Gawain()
+        network_interfaces = Gawain()
+        network_interfaces = processor._networkdata()
+        self.assertTrue("eth0" in network_interfaces)
